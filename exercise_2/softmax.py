f5ddb775edbdc53bab8d9f751e589005b92a484b
jostlowe <jostlowe@gmail.com>
2019-02-11 09:05:51 +0100

    It FeCkiN wOrKs

--- exercise_2/softmax.py
+++ exercise_2/softmax.py
@@ -93,8 +93,11 @@ def softmax(a):
     a_exp = np.exp(a)
     return a_exp / a_exp.sum(axis=1, keepdims=True)
 
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
 
-def forward(X, w):
+
+def forward(X, w1, w2):
     """
     Performs a forward pass through the network
     --
@@ -103,11 +106,13 @@ def forward(X, w):
     --
     Returns: [batch_size, num_classes] numpy vector
     """
-    a = X.dot(w.T)
-    return softmax(a)
+    a1 = sigmoid(X.dot(w1.T))
+    a2 = softmax(a1.dot(w2.T))
+
+    return a2
 
 
-def calculate_accuracy(X, targets, w):
+def calculate_accuracy(X, targets, w1, w2):
     """
     Calculated the accuracy of the network.
     ---
@@ -117,13 +122,13 @@ def calculate_accuracy(X, targets, w):
     --
     Returns float
     """
-    output = forward(X, w)
+    output = forward(X, w1, w2)
     predictions = output.argmax(axis=1)
     targets = targets.argmax(axis=1)
     return (predictions == targets).mean()
 
 
-def cross_entropy_loss(X, targets, w):
+def cross_entropy_loss(X, targets, w1, w2):
     """
     Computes the cross entropy loss given the input vector X and the target vector.
     ---
@@ -133,14 +138,14 @@ def cross_entropy_loss(X, targets, w):
     --
     Returns float
     """
-    output = forward(X, w)
+    output = forward(X, w1, w2)
     assert output.shape == targets.shape 
     log_y = np.log(output)
     cross_entropy = -targets * log_y
     return cross_entropy.mean()
 
 
-def gradient_descent(X, targets, w, learning_rate, should_check_gradient):
+def gradient_descent(X, targets, w1, w2, learning_rate, should_check_gradient):
     """
     Performs gradient descents for all weights in the network.
     ---
@@ -155,24 +160,34 @@ def gradient_descent(X, targets, w, learning_rate, should_check_gradient):
     # If you take loss.sum(), the normalization factor is 1.
     # The normalization factor is identical for all weights in the network (For multi-layer neural-networks as well.)
     normalization_factor = X.shape[0] * targets.shape[1] # batch_size * num_classes
-    outputs = forward(X, w)
+    outputs = forward(X, w1, w2)
     delta_k = - (targets - outputs)
+    a = sigmoid(X.dot(w1.T))
 
-    dw = delta_k.T.dot(X)
-    dw = dw / normalization_factor  # Normalize gradient equally as we do with the loss
-    assert dw.shape == w.shape, "dw shape was: {}. Expected: {}".format(dw.shape, w.shape)
+    inside_parentheses = np.multiply(np.multiply(a, 1-a), np.dot(delta_k, w2))
+
+    dw1 = np.dot(inside_parentheses.T, X)
+    dw2 = delta_k.T.dot(a)
+
+    dw1 = dw1 / normalization_factor  # Normalize gradient equally as we do with the loss
+    dw2 = dw2 / normalization_factor  # Normalize gradient equally as we do with the loss
+    assert dw1.shape == w1.shape, "dw shape was: {}. Expected: {}".format(dw1.shape, w1.shape)
+    assert dw2.shape == w2.shape, "dw shape was: {}. Expected: {}".format(dw2.shape, w2.shape)
 
     if should_check_gradient:
-        check_gradient(X, targets, w, 1e-2,  dw)
+        check_gradient(X, targets, w1, 1e-2,  dw1)
+        check_gradient(X, targets, w2, 1e-2,  dw2)
 
-    w = w - learning_rate * dw
-    return w
+    w1 = w1 - learning_rate * dw1
+    w2 = w2 - learning_rate * dw2
+    return w1, w2
 
-mnist.init()
+
+#mnist.init()
 X_train, Y_train, X_test, Y_test = mnist.load()
 
 # Pre-process data
-X_train, X_test = X_train / 255, X_test / 255
+X_train, X_test = (X_train/127.5)-1, (X_test/127.5)-1
 X_train = bias_trick(X_train)
 X_test = bias_trick(X_test)
 Y_train, Y_test = onehot_encode(Y_train), onehot_encode(Y_test)
@@ -181,12 +196,13 @@ X_train, Y_train, X_val, Y_val = train_val_split(X_train, Y_train, 0.1)
 
 
 # Hyperparameters
-batch_size = 64
+batch_size = 128
 learning_rate = 0.5
 num_batches = X_train.shape[0] // batch_size
 should_gradient_check = False
 check_step = num_batches // 10
 max_epochs = 20
+layer_sizes = [785, 64, 10]
 
 # Tracking variables
 TRAIN_LOSS = []
@@ -198,58 +214,60 @@ VAL_ACC = []
 
 
 def train_loop():
-    w = np.zeros((Y_train.shape[1], X_train.shape[1]))
+    w1 = np.random.uniform(-1, 1, (64, X_train.shape[1]))
+    w2 = np.random.uniform(-1, 1, (Y_train.shape[1], 64))
+    '''
     for e in range(max_epochs):  # Epochs
         for i in tqdm.trange(num_batches):
             X_batch = X_train[i*batch_size:(i+1)*batch_size]
             Y_batch = Y_train[i*batch_size:(i+1)*batch_size]
 
-            w = gradient_descent(X_batch,
+            w1, w2 = gradient_descent(X_batch,
                                  Y_batch,
-                                 w,
+                                 w1, w2,
                                  learning_rate,
                                  should_gradient_check)
             if i % check_step == 0:
                 # Loss
-                TRAIN_LOSS.append(cross_entropy_loss(X_train, Y_train, w))
-                TEST_LOSS.append(cross_entropy_loss(X_test, Y_test, w))
-                VAL_LOSS.append(cross_entropy_loss(X_val, Y_val, w))
+                TRAIN_LOSS.append(cross_entropy_loss(X_train, Y_train, w1, w2))
+                TEST_LOSS.append(cross_entropy_loss(X_test, Y_test, w1, w2))
+                VAL_LOSS.append(cross_entropy_loss(X_val, Y_val, w1, w2))
 
-                TRAIN_ACC.append(calculate_accuracy(X_train, Y_train, w))
-                VAL_ACC.append(calculate_accuracy(X_val, Y_val, w))
-                TEST_ACC.append(calculate_accuracy(X_test, Y_test, w))
+                TRAIN_ACC.append(calculate_accuracy(X_train, Y_train, w1, w2))
+                VAL_ACC.append(calculate_accuracy(X_val, Y_val, w1, w2))
+                TEST_ACC.append(calculate_accuracy(X_test, Y_test, w1, w2))
                 if should_early_stop(VAL_LOSS):
                     print(VAL_LOSS[-4:])
                     print("early stopping.")
-                    return w
-    return w
+                    return w1, w2
+        '''
+    return w1, w2
 
 
-w = train_loop()
-
+w1, w2 = train_loop()
 plt.plot(TRAIN_LOSS, label="Training loss")
 plt.plot(TEST_LOSS, label="Testing loss")
 plt.plot(VAL_LOSS, label="Validation loss")
 plt.legend()
-plt.ylim([0, 0.05])
+plt.ylim([0, 0.15])
 plt.show()
 
 plt.clf()
 plt.plot(TRAIN_ACC, label="Training accuracy")
 plt.plot(TEST_ACC, label="Testing accuracy")
 plt.plot(VAL_ACC, label="Validation accuracy")
-plt.ylim([0.8, 1.0])
+plt.ylim([0.5, 1.0])
 plt.legend()
 plt.show()
 
 plt.clf()
-w = w[:, :-1]  # Remove bias
-w = w.reshape(10, 28, 28)
-w = np.concatenate(w, axis=0)
+'''
+w2 = w2[:, :-1]  # Remove bias
+w2 = w2.reshape(10, 28, 28)
+w = np.concatenate(w2, axis=0)
 plt.imshow(w, cmap="gray")
 plt.show()
-
-
+'''
 
 
 
